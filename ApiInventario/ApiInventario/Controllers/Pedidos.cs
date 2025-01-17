0da7d2d61724069309b1ac5107fc2ba80933bfbd
asd <asd>
2025-01-17 08:45:30 -0500

    Se agregan controladores

---
+++ ApiInventario/ApiInventario/Controllers/Pedidos.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiInventario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Pedidos : ControllerBase
+    {
+    }
+}
